6cd3c8a034cc7a4edcc2c13c864794941e16b093
silver__toad <den.shaikhatarov@gmail.com>
2026-01-04 13:01:37 +0200

    feat: move file path input from runtime to launch parameters/arguments

--- src/cli.py
+++ src/cli.py
@@ -1,11 +1,18 @@
+import argparse
 import sys
 from src.parser import parse_shared_object_file
 
 def main():
-    file_path = input("Type the path to the .so file: ")
+    parser = argparse.ArgumentParser(
+        prog="Shared Object Lister",
+        description="List available native functions from a Linux .so file.")
+    parser.add_argument(
+        "file_path",
+        help="Absolute path to the shared object (.so) file")
+    args = parser.parse_args()
 
     try:
-        functions = parse_shared_object_file(file_path)
+        functions = parse_shared_object_file(args.file_path)
         for func in functions:
             print_function(func)
     except FileNotFoundError:
