eabf2d87a3f7318c412149d76f2e90d5b58ea15f
silver__toad <den.shaikhatarov@gmail.com>
2026-01-04 12:04:43 +0200

    feat: separate input, output, and main into separate scripts and added path input

---
+++ src/cli.py
@@ -0,0 +1,14 @@
+import sys
+from src.parser import parse_shared_object_file
+
+def main():
+    file_path = input("Type the path to the .so file: ")
+
+    try:
+        parse_shared_object_file(file_path)
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.", file=sys.stderr)
+        sys.exit(1)
+    except Exception as e:
+        print(f"Error: Failed to parse file. {e}", file=sys.stderr)
+        sys.exit(1)
